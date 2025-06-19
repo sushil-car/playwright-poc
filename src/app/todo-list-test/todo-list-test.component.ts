514224fd5bf7a628e65e5baa8bccb31e08dbd674
Sushil Adokar <sushil_adokar@bmc.com>
2025-06-19 21:33:20 +0530

    Added todo component

---
+++ src/app/todo-list-test/todo-list-test.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-list-test',
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './todo-list-test.component.html',
+  styleUrl: './todo-list-test.component.scss'
+})
+export class TodoListTestComponent {
+  todos: string[] = [];
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.todos.push(this.newTodo.trim());
+      this.newTodo = '';
+    }
+  }
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  toggleTodo(index: number) {
+    // Implement toggle functionality if needed
+  }
+
+  clearTodos() {
+    this.todos = [];
+  }
+  getTodoCount(): number {
+    return this.todos.length;
+  }
+
+  isTodoListEmpty(): boolean {
+    return this.todos.length === 0;
+  }
+
+  getTodoList(): string[] {
+    return this.todos;
+  }
+
+  getNewTodo(): string {
+    return this.newTodo;
+  }
+
+  setNewTodo(value: string) {
+    this.newTodo = value;
+  }
+}
